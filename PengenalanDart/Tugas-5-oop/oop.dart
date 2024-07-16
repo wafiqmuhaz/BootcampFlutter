0b9ead89daacfd350f4d496a12cb0583e796becb
Wafiq Muhammad Abdul Aziz <wafiqmuhaz@gmail.com>
2024-07-16 11:26:28 +0700

    Modify Folder Structures

---
+++ PengenalanDart/Tugas-5-oop/oop.dart
@@ -0,0 +1,14 @@
+
+import 'soalSatu.dart';
+import 'soalDua.dart';
+import 'soalTiga.dart';
+import 'soalEmpat.dart';
+import 'soalLima.dart';
+
+void main(){
+  soalkeSatu();
+  soalkeDua();
+  soalkeTiga();
+  soalkeEmpat();
+  soalkeLima();
+}
